dc5b395d9d0099e4a8acd463084df7162410bc32
Steven P <stevu@gemidyne.net>
2020-04-29 10:53:11 +0100

    Ensure all folders are used when templating
    Also updated to rely on settings from settings manifest rather than viewmodel

--- Tsukuru.NetCore/Maps/Compiler/Business/CompileSteps/ResourcePackingStep.cs
+++ Tsukuru.NetCore/Maps/Compiler/Business/CompileSteps/ResourcePackingStep.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using System.Linq;
-using GalaSoft.MvvmLight.Ioc;
 using Tsukuru.Maps.Compiler.ViewModels;
 using Tsukuru.Maps.Packer;
 using Tsukuru.Settings;
@@ -13,19 +12,19 @@ namespace Tsukuru.Maps.Compiler.Business.CompileSteps
 
         public override bool Run(ResultsLogContainer log)
         {
-            var viewModel = SimpleIoc.Default.GetInstance<ResourcePackingViewModel>();
+            var folders = SettingsManager.Manifest.MapCompilerSettings.ResourcePackingSettings.Folders;
 
             var session = new PackerSessionDetails
             {
                 MapFile = MapCompileSessionInfo.Instance.GeneratedBspFile.FullName,
                 GamePath = SdkToolsPath,
                 CompleteFoldersToAdd =
-                    viewModel.FoldersToPack.Where(x => !x.Intelligent).Select(x => x.Folder).ToList(),
+                    folders.Where(x => !x.Intelligent).Select(x => x.Path).ToList(),
                 IntelligentFoldersToAdd =
-                    viewModel.FoldersToPack.Where(x => x.Intelligent).Select(x => x.Folder).ToList()
+                    folders.Where(x => x.Intelligent).Select(x => x.Path).ToList()
             };
 
-            if (viewModel.FoldersToPack.Select(x => x.Folder).Any(x => !Directory.Exists(x)))
+            if (folders.Select(x => x.Path).Any(x => !Directory.Exists(x)))
             {
                 log.AppendLine("ResourcePackingStep", "Unable to start packing. A directory specified does not exist.");
                 return false;
@@ -35,9 +34,9 @@ namespace Tsukuru.Maps.Compiler.Business.CompileSteps
 
             if (SettingsManager.Manifest.MapCompilerSettings.ResourcePackingSettings.GenerateMapSpecificFiles)
             {
-                var folders = session.CompleteFoldersToAdd.Concat(session.IntelligentFoldersToAdd).ToList();
+                var allFolders = session.CompleteFoldersToAdd.Concat(session.IntelligentFoldersToAdd).ToList();
 
-                using (var mapSpecificFileGenerator = new TemplatingEngine(log, folders, MapCompileSessionInfo.Instance.MapName))
+                using (var mapSpecificFileGenerator = new TemplatingEngine(log, allFolders, MapCompileSessionInfo.Instance.MapName))
                 {
                     mapSpecificFileGenerator.Generate();
 
