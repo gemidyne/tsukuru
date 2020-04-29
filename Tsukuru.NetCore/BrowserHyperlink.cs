cee8a4528c3add33910ca6237fbe7ee6c4cbe59e
Steven P <stevu@gemidyne.net>
2020-04-29 10:56:16 +0100

    Fixed browserhyperlink not opening web browsers properly

--- Tsukuru.NetCore/BrowserHyperlink.cs
+++ Tsukuru.NetCore/BrowserHyperlink.cs
@@ -1,20 +1,36 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows.Documents;
 using System.Windows.Navigation;
 
 namespace Tsukuru
 {
-	public class BrowserHyperlink : Hyperlink
-	{
-		public BrowserHyperlink()
-		{
-			RequestNavigate += OnRequestNavigate;
-		}
+    public class BrowserHyperlink : Hyperlink
+    {
+        public BrowserHyperlink()
+        {
+            RequestNavigate += OnRequestNavigate;
+        }
 
-		private void OnRequestNavigate(object sender, RequestNavigateEventArgs e)
-		{
-			Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
-			e.Handled = true;
-		}
-	}
+        private void OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            string url = e.Uri.AbsoluteUri;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+
+            e.Handled = true;
+        }
+    }
 }
